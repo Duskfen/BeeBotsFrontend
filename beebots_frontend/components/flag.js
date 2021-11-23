32b55ec0898b95986b48740bda62c63dd651743c
Gabriel Lugmayr <gabriel@waldviertelblick.at>
2021-11-23 18:55:58 +0100

    improved falg

--- beebots_frontend/components/flag.js
+++ beebots_frontend/components/flag.js
@@ -4,14 +4,12 @@ import Box from './box'
 
 export default function Flag({ children, style, setShowFlag, flagType }) {
 
-   setTimeout(() => setShowFlag(false), 100000);
-
-   //todo flag
+   setTimeout(() => setShowFlag(false), 8000);
 
    return (
       <div className={[styles.flag, styles[flagType]].join(" ")} style={style}>
          <Box>
-            <p>this is a {flagType} flag</p>
+            <p>this is a {flagType} flag theres some more message going on here</p>
          </Box>
       </div>
    )
