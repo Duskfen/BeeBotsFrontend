47b6b05c5fdcdaf8692aa60f09d0996df66cb9e1
Gabriel Lugmayr <gabriel@waldviertelblick.at>
2022-01-10 15:22:19 +0100

    mobile optimizations

--- beebots_frontend/components/layout.js
+++ beebots_frontend/components/layout.js
@@ -1,20 +1,23 @@
-import Head from 'next/head'
-import Navbar from '../components/navbar';
-import styles from './layout.module.scss'
-
+import Head from "next/head";
+import Navbar from "../components/navbar";
+import styles from "./layout.module.scss";
 
 export default function Layout({ children }) {
-   return (
-      <>
-         <Head>
-            <title>Beebots</title>
-            <meta name="description" content="Beebots are automated trading Algorithms" />
-            <link rel="icon" href="/favicon.ico" />
-         </Head>
-            <Navbar>
-            </Navbar>
-            
-         <main className={styles.restrict + " " + styles.mainbody}>{children}</main>
-      </>
-   )
+  return (
+    <>
+      <Head>
+        <title>Beebots</title>
+        <meta
+          name="description"
+          content="Beebots are automated trading Algorithms"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar>
+        <main className={styles.restrict + " " + styles.mainbody}>
+          {children}
+        </main>
+      </Navbar>
+    </>
+  );
 }
--- beebots_frontend/components/navbar.js
+++ beebots_frontend/components/navbar.js
@@ -1,47 +1,93 @@
-
-import styles from './navbar.module.scss'
-import LayoutStyles from './layout.module.scss'
-import Link from 'next/link'
-import Beelogo from '../components/beelogo';
-import Box from '../components/box'
-import { useRouter } from 'next/router'
+import styles from "./navbar.module.scss";
+import LayoutStyles from "./layout.module.scss";
+import Link from "next/link";
+import Beelogo from "../components/beelogo";
+import Box from "../components/box";
+import { useRouter } from "next/router";
+import useMediaQuery from "../utils/useMediaQuery";
 
 const menu = [
-   { title: 'Home', path: '/bot'},
-   { title: 'Styleguide', path: '/styleguide' },
-   { title: 'Credits', path: '/credits' }
-]
+  { title: "Home", path: "/bot" },
+  { title: "Styleguide", path: "/styleguide" },
+  { title: "Credits", path: "/credits" },
+];
 
 export default function Navbar({ children }) {
-   const router = useRouter()
+  const router = useRouter();
+  const isDesktop = useMediaQuery("(min-width: 600px)");
 
-   return (
-      <>
-         <div className={styles.sticky}>
-            <Box>
-               <nav>
-                  <div className={LayoutStyles.restrict}>
-
-                     <div className={styles.nav}>
-                        <div className={styles.Beelogo}><Beelogo height="80px" width="80px" /></div>
-                        <ul>
-                           {menu.map(({ title, path }) => {
-                              return (
-                                 <li key={`nav_${path}`}>
-                                    <Link href={path}><a className={`${router.pathname === path
-                                          ? styles.activeLink
-                                          : null
-                                       }`}>{title}</a></Link>
-                                 </li>
-                              )
-                           })}
-                        </ul>
-                     </div>
+  return (
+    <>
+      {isDesktop ? (
+        <>
+          <div className={styles.stickybottom}>
+            <Box className={styles.navbox}>
+              <nav>
+                <div className={LayoutStyles.restrict}>
+                  <div className={styles.nav}>
+                    <div className={styles.Beelogo}>
+                      <Beelogo height="80px" width="80px" />
+                    </div>
+                    <ul>
+                      {menu.map(({ title, path }) => {
+                        return (
+                          <li key={`nav_${path}`}>
+                            <Link href={path}>
+                              <a
+                                className={`${
+                                  router.pathname === path
+                                    ? styles.activeLink
+                                    : null
+                                }`}
+                              >
+                                {title}
+                              </a>
+                            </Link>
+                          </li>
+                        );
+                      })}
+                    </ul>
                   </div>
-               </nav>
+                </div>
+              </nav>
             </Box>
-         </div>
-         {children}
-      </>
-   )
+          </div>
+          {children}
+        </>
+      ) : (
+        <>
+          {children}
+          <div className={styles.bottomsticky}>
+            <Box className={styles.navbox}>
+              <nav>
+                <div className={LayoutStyles.restrict}>
+                  <div className={styles.nav}>
+                    <ul>
+                      {menu.map(({ title, path }) => {
+                        return (
+                          <li key={`nav_${path}`}>
+                            <Link href={path}>
+                              <a
+                                className={`${
+                                  router.pathname === path
+                                    ? styles.activeLink
+                                    : null
+                                }`}
+                              >
+                                {title}
+                              </a>
+                            </Link>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+            </Box>
+          </div>
+        </>
+      )}
+    </>
+  );
 }
