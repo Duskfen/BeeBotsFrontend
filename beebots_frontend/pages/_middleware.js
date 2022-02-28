d2e8d58a9032fbaf0212ec51ab5847a57d43a040
Gabriel Lugmayr <gabriel@waldviertelblick.at>
2022-02-28 15:50:16 +0100

    fix and vis

--- beebots_frontend/pages/_middleware.js
+++ beebots_frontend/pages/_middleware.js
@@ -2,7 +2,7 @@ import { NextResponse, NextRequest } from 'next/server'
 export async function middleware(req, ev) {
     const { pathname } = req.nextUrl
     if (pathname == '/') {
-        return NextResponse.redirect('/bot')
+        return NextResponse.redirect(new URL('/bot', req.url))
     }
     return NextResponse.next()
 }
--- beebots_frontend/pages/bot/[botname].js
+++ beebots_frontend/pages/bot/[botname].js
@@ -15,6 +15,8 @@ export default function Bots({ details, botname, botsId, details7d }) {
       drawNormalProfitChart(details7d);
     }
 
+    drawTradesChart();
+
     return () => {
       console.log("Component onUnmount");
     };
@@ -27,8 +29,126 @@ export default function Bots({ details, botname, botsId, details7d }) {
       </div>
     );
   }
-  console.log(botdetails);
-  console.log(details7d);
+
+  function drawTradesChart() {
+    // set the dimensions and margins of the graph
+    const width = 450,
+      height = 450,
+      margin = 50;
+
+    // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+    const radius = Math.min(width, height) / 2 - margin;
+
+    d3.select("#myTradesChart").selectAll("*").remove();
+
+    const svg = d3
+      .select("#myTradesChart")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", `translate(${width / 2},${height / 2})`);
+
+      console.log(botdetails)
+
+    let data = botdetails.map((d) => {
+      return {
+        success:  d.countCorrect+1,
+        fail:  d.countWrong+1,
+        even:  d.countBreakEven+1,
+      };
+    }); //success: 3, fail: 1, even:1
+
+    data = data.reduce((a, b) => {
+      return {
+        success: a.success + b.success,
+        fail: a.fail + b.fail,
+        even: a.even + b.even,
+      };
+    });
+
+
+    console.log(data);
+
+    const color = d3
+      .scaleOrdinal()
+      .domain(["success", "fail", "even"])
+      .range(["#27ae60", "#e74c3c", "#f1c40f"]);
+
+    // Compute the position of each group on the pie:
+    const pie = d3
+      .pie()
+      .sort(null) // Do not sort group by size
+      .value((d) => d[1]);
+    const data_ready = pie(Object.entries(data));
+
+    // The arc generator
+    const arc = d3
+      .arc()
+      .innerRadius(radius * 0.5) // This is the size of the donut hole
+      .outerRadius(radius * 0.8);
+
+    // Another arc that won't be drawn. Just for labels positioning
+    const outerArc = d3
+      .arc()
+      .innerRadius(radius * 0.9)
+      .outerRadius(radius * 0.9);
+
+    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    svg
+      .selectAll("allSlices")
+      .data(data_ready)
+      .join("path")
+      .attr("d", arc)
+      .attr("fill", (d) => {
+         if(d.data[0] ==="success"){
+            return "#27ae60";
+         }
+         else if (d.data[0] ==="fail"){
+            return "#e74c3c";
+         }
+         else if (d.data[0] ==="even"){
+            return "#f1c40f";
+         }
+      })
+      .attr("stroke", "white")
+      .style("stroke-width", "2px")
+      .style("opacity", 0.7);
+
+    // Add the polylines between chart and labels:
+    svg
+      .selectAll("allPolylines")
+      .data(data_ready)
+      .join("polyline")
+      .attr("stroke", "black")
+      .style("fill", "none")
+      .attr("stroke-width", 1)
+      .attr("points", function (d) {
+        const posA = arc.centroid(d); // line insertion in the slice
+        const posB = outerArc.centroid(d); // line break: we use the other arc generator that has been built only for that
+        const posC = outerArc.centroid(d); // Label position = almost the same as posB
+        const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2; // we need the angle to see if the X position will be at the extreme right or extreme left
+        posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+        return [posA, posB, posC];
+      });
+
+    // Add the polylines between chart and labels:
+    svg
+      .selectAll("allLabels")
+      .data(data_ready)
+      .join("text")
+      .text((d) => d.data[0])
+      .attr("transform", function (d) {
+        const pos = outerArc.centroid(d);
+        const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2;
+        pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
+        return `translate(${pos})`;
+      })
+      .style("text-anchor", function (d) {
+        const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2;
+        return midangle < Math.PI ? "start" : "end";
+      });
+  }
 
   function drawNormalProfitChart(data) {
     // set the dimensions and margins of the graph
@@ -37,7 +157,7 @@ export default function Bots({ details, botname, botsId, details7d }) {
       height = 400 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
-    d3.select("#myNormalProfitChartt").selectAll("*").remove();
+    d3.select("#myNormalProfitChart").selectAll("*").remove();
 
     const svg = d3
       .select("#myNormalProfitChart")
@@ -82,9 +202,9 @@ export default function Bots({ details, botname, botsId, details7d }) {
       .style("padding", "10px");
 
     // Three function that change the tooltip when user hover / move / leave a cell
-    var mouseover = function (event,d) {
+    var mouseover = function (event, d) {
       tooltip
-        .html( "<p>Value: " + d.totalProfit*100 + "%</p>")
+        .html("<p>Value: " + d.totalProfit * 100 + "%</p>")
         .style("opacity", 1);
     };
     var mousemove = function (d) {
@@ -97,24 +217,24 @@ export default function Bots({ details, botname, botsId, details7d }) {
     };
 
     svg
-    .selectAll("mybar")
-    .data(data)
-    .join("rect")
-    .attr("x", (d) => x(d.date))
-    .attr("y", (d) => y(d.totalProfit * 100))
-    .attr("width", x.bandwidth())
-    .attr("height", (d) => height - y(d.totalProfit))
-    .attr("fill", (d) => {
-      if (d.totalProfit > 0) {
-        return "#27ae60";
-      }
-      if (d.totalProfit < 0) {
-        return "#e74c3c";
-      } else return "#f1c40f";
-    })
-    .on("mouseover", mouseover)
-    .on("mousemove", mousemove)
-    .on("mouseleave", mouseleave)
+      .selectAll("mybar")
+      .data(data)
+      .join("rect")
+      .attr("x", (d) => x(d.date))
+      .attr("y", (d) => y(d.totalProfit * 100))
+      .attr("width", x.bandwidth())
+      .attr("height", (d) => height - y(d.totalProfit))
+      .attr("fill", (d) => {
+        if (d.totalProfit > 0) {
+          return "#27ae60";
+        }
+        if (d.totalProfit < 0) {
+          return "#e74c3c";
+        } else return "#f1c40f";
+      })
+      .on("mouseover", mouseover)
+      .on("mousemove", mousemove)
+      .on("mouseleave", mouseleave);
   }
 
   return (
@@ -132,15 +252,15 @@ export default function Bots({ details, botname, botsId, details7d }) {
         >
           <div className={styles.GeneralBoxRow}>
             <p>Profit:</p>
-            <p>{botdetails[0].totalProfit * 10}%</p>
+            <p>{botdetails[0]?.totalProfit * 10}%</p>
           </div>
 
           <div className={styles.GeneralBoxRow}>
             <p>Trades:</p>
             <p>
-              {botdetails[0].countWrong +
-                botdetails[0].countCorrect +
-                botdetails[0].countBreakEven}
+              {botdetails[0]?.countWrong +
+                botdetails[0]?.countCorrect +
+                botdetails[0]?.countBreakEven}
             </p>
           </div>
 
@@ -158,9 +278,17 @@ export default function Bots({ details, botname, botsId, details7d }) {
         <DashboardItem
           title={"General"}
           onTimeSpanChange={(e) => console.log("timespan", e)}
+          onTimeSpan2Change={(e) => console.log("timespan", e)}
           use2Times={true}
         >
           <div id="myNormalProfitChart"></div>
+        </DashboardItem>
+
+        <DashboardItem
+          title={"Trades"}
+          onTimeSpanChange={(e) => console.log("timespan", e)}
+        >
+          <div id="myTradesChart"></div>
         </DashboardItem>
       </div>
     </>
